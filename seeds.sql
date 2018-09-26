f9ca9433a4eadd5a5c733ba236a980cb2e05c7d9
Julie Groth <groth.julie@gmail.com>
2018-09-25 22:13:45 -0700

    updated seeds file

--- seeds.sql
+++ seeds.sql
@@ -1,17 +1,17 @@
 -- initial table values
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, product_sales)
 VALUES 
-(100,"Laptop","Computers",917.63,2),
-(272,"Blanket","Kids",65.86,76),
-(325,"Frozen","Movies",20.35,19),
-(415,"Harry Potter","Books",18.83,4),
-(465,"Bandaids","Health",2.87,48),
-(530,"Bottle","Baby",8.36,38),
-(765,"Pasta Lasagne Fresh","Grocery",3.17,33),
-(804,"Lipstick","Beauty",5.12,25),
-(805,"Yahtzee","Games",7.23,28),
-(924,"Thermometer","Health",83.12,83);
+(100,"Laptop","Computers",917.63,2,1000),
+(272,"Blanket","Kids",65.86,76,1000),
+(325,"Frozen","Movies",20.35,19,1000),
+(415,"Harry Potter","Books",18.83,4,1000),
+(465,"Bandaids","Health",2.87,48,1000),
+(530,"Bottle","Baby",8.36,38,1000),
+(765,"Pasta Lasagne Fresh","Grocery",3.17,33,1000),
+(804,"Lipstick","Beauty",5.12,25,1000),
+(805,"Yahtzee","Games",7.23,28,1000),
+(924,"Thermometer","Health",83.12,83,1000);
 
 INSERT INTO departments (department_name, over_head_costs)
 VALUES
